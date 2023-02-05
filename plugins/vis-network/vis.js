536478e2f793669dbbf5b6a02d22ba3e0efcd3cf
miRoox <miroox@outlook.com>
2023-02-05 13:06:04 +0800

    plugins: install TiddlyMap and setup
    * https://github.com/felixhayashi/TW5-TopStoryView/commit/2b2e7090c2b368dd23749e94d46ed4c4d12eba20 (10/10/2015)
    * https://github.com/felixhayashi/TW5-HotZone/commit/30c17b8b4cdb096b94493804626317ac72bd54d4 (7/25/2021)
    * https://github.com/flibbles/tw5-vis-network/commit/0bbab0306faf10f6cd6d653d5fda4d2630981084 (9/7/2022)
    * https://github.com/felixhayashi/TW5-TiddlyMap/commit/94eebccfafeb9556586294347407b9f0464c2a79 (10/29/2022)

---
+++ plugins/vis-network/vis.js
@@ -0,0 +1,43 @@
+/*\
+title: $:/plugins/flibbles/vis-network/vis.js
+type: application/javascript
+module-type: library
+
+*** TO AVOID STRANGE LIB ERRORS FROM BUBBLING UP *****************
+\*/
+
+if($tw.boot.tasks.trapErrors) {
+
+  var defaultHandler = window.onerror;
+  window.onerror = function(errorMsg, url, lineNumber) {
+    
+    if(errorMsg.indexOf("NS_ERROR_NOT_AVAILABLE") !== -1
+       && url == "$:/plugins/flibbles/vis-network/vis-network.min.js") {
+         
+      var text = "Strange firefox related vis.js error (see #125)";
+      console.error(text, arguments);
+      
+    } else if(errorMsg.indexOf("Permission denied to access property") !== -1) {
+      
+      var text = "Strange firefox related vis.js error (see #163)";
+      console.error(text, arguments);
+      
+    } else if(defaultHandler) {
+      
+      defaultHandler.apply(this, arguments);
+      
+    }
+    
+  }
+  
+}
+
+module.exports = require("$:/plugins/flibbles/vis-network/vis-network.min.js");
+
+if ($tw.browser && CanvasRenderingContext2D.prototype.circle === undefined) {
+	// For some reason, vis-network thinks this method exists. It don't.
+	// We've got to add it ourselves if it's not there.
+	CanvasRenderingContext2D.prototype.circle = function(x, y, radius) {
+		return this.arc(x, y, radius, 0, 2 * Math.PI);
+	};
+}
