cb843e4efdd432c22e297a1471abe0b67dd0c910
miRoox <miroox@outlook.com>
2024-06-09 02:44:33 +0000

    Deploying to gh-pages from @ miRoox/wiki@016a588e385743175c9fedd4552e561264dffaae 🚀

---
+++ start.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const tiddlywiki = require("tiddlywiki");
+
+const tw = tiddlywiki.TiddlyWiki();
+
+const host = process.env.HOST
+const port = process.env.WIKIPORT ?? '7777'
+const password = process.env.WIKIPW
+
+const argv = ['--listen']
+
+if (host) {
+    argv.push('host='+host)
+}
+argv.push('port='+port)
+if (password) {
+    argv.push('username=miRoox', 'password='+password)
+}
+argv.push('readers=(anon)', 'writers=(authenticated)', 'admin=miRoox')
+
+console.debug('arg: %o', argv)
+
+tw.boot.argv = argv
+
+tw.boot.boot();
