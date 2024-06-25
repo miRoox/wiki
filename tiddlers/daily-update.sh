773bab2d47217971a032259bd3ee5c320725ba3d
miRoox <miroox@outlook.com>
2024-06-25 06:29:05 +0000

    Deploying to gh-pages from @ miRoox/wiki@967aa35bcf923863783de31e735adfca624bac30 🚀

---
+++ tiddlers/daily-update.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+## on Linux
+# crontab -e
+# 0 21 * * * /path/to/MyWiki/tiddlers/daily-update.sh
+
+## on Windows
+# schtasks /create /tn "WikiDailyUpdate" /tr "\path\to\MyWiki\tiddlers\daily-update.sh" /sc daily /st 23:00
+
+set -e
+
+cd "$(dirname "$0")"
+FOLDER=.
+
+git pull --ff-only
+if git diff --exit-code "$FOLDER"; then
+    echo "No updates today"
+else
+    git add "$FOLDER"
+    git commit --no-gpg-sign -m "Update on $(date --utc -I) from $(hostname)"
+    git push
+fi
